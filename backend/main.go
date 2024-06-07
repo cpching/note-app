b976889036d874026f9bbe5305fbfcb1d0f00db5
Pin-Ching Chen <p.ching0811@gmail.com>
2024-06-07 20:54:30 +0800

    chore(backend): remove unused code

--- backend/main.go
+++ backend/main.go
@@ -10,19 +10,6 @@ import (
     "github.com/rs/cors"
 )
 
-func enableCors(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-        if r.Method == "OPTIONS" {
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
-}
-
-
 func main() {
     initDB()
 
