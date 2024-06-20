e6cf46033e61f93f8cfab6086ef40abc8b1c036e
Pin-Ching Chen <p.ching0811@gmail.com>
2024-06-20 21:29:36 +0800

    chore: update database username and adjust formatting in `backend/handlers.go`

--- backend/db.go
+++ backend/db.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-    "database/sql"
-    "log"
+	"database/sql"
+	"log"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
-func initDB()  {
-    var err error
-    if db, err = sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/note_app"); err != nil {
-        log.Fatal(err)
-    } 
-    if err = db.Ping(); err != nil {
-        log.Fatal(err)
-    }
+func initDB() {
+	var err error
+	if db, err = sql.Open("mysql", "goBackend:password@tcp(127.0.0.1:3306)/note_app"); err != nil {
+		log.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Print("!")
+		log.Fatal(err)
+	}
 }
-
-
